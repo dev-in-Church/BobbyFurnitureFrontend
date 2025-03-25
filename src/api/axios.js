620c8363df120c98a5bc68b99154838c773cdfa8
Mannu404 <ambundoemmanuel@gmail.com>
2025-03-25 15:47:58 +0300

    lh:5000 to render in axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000", //"https://bobbyfurnitureonline.onrender.com", // Change to your backend URL
+  baseURL: "https://bobbyfurnitureonline.onrender.com", // Change to your backend URL
 });
 
 // Optional: Add an interceptor to attach the JWT token to every request
