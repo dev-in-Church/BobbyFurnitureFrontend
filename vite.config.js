717f6f76d61bcbaddb0cd01072aee6c635182fcf
dev-in-Church <mannuitman@gmail.com>
2026-05-18 00:57:43 +0300

    pwa

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,50 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 import path from "path";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+
+    VitePWA({
+      registerType: "autoUpdate",
+
+      manifest: {
+        name: "Your App Name",
+        short_name: "YourApp",
+        description: "My React Vite PWA",
+        theme_color: "#000000",
+        background_color: "#ffffff",
+        display: "standalone",
+        start_url: "/",
+
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+
   define: {
     global: "globalThis",
   },
+
   optimizeDeps: {
     exclude: ["next"],
   },
